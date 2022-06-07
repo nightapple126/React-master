cbadf66674c85c63aa32fcf03347e4e59b98aed0
nightapple <nightapple126@gmail.com>
2022-06-07 22:17:51 +0800

    react-work

---
+++ getting-started-react-typescript/src/projects/ProjectsPage.tsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react';
+import ProjectList from './ProjectList';
+import { loadProjects } from './state/projectActions';
+import { useSelector, useDispatch } from 'react-redux';
+import { AppState } from '../state';
+import { Dispatch } from "redux";
+
+function ProjectsPage() {
+  const loading = useSelector(
+    (appState: AppState) => appState.projectState.loading
+  );
+  const projects = useSelector(
+    (appState: AppState) => appState.projectState.projects
+  );
+  const error = useSelector(
+    (appState: AppState) => appState.projectState.error
+  );
+  const currentPage = useSelector(
+    (appState: AppState) => appState.projectState.page
+  );
+  const dispatch: Dispatch<any> = useDispatch();
+  useEffect(() => {
+    dispatch(loadProjects(1));
+  }, [dispatch]);
+
+  const handleMoreClick = () => {
+      dispatch(loadProjects(currentPage + 1));
+    };
+  return (
+    <>
+      <h1>Projects</h1> 
+      {error && (
+         <div className="row">
+           <div className="card large error">
+             <section>
+               <p>
+                 <span className="icon-alert inverse "></span>
+                 {error}
+               </p>
+             </section>
+           </div>
+         </div>
+       )}
+      <div>
+        <ProjectList 
+        projects={projects} />
+        {!loading && !error && (
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="button-group fluid">
+                <button className="button default" onClick={handleMoreClick}>
+                  More...
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+        {loading && (
+          <div className="center-page">
+            <span className="spinner primary"></span>
+            <p>Loading...</p>
+          </div>
+        )}
+      </div>       
+    </>
+   );
+}
+
+export default ProjectsPage;
