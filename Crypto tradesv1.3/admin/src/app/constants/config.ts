3e4f11752883d05f5f522db99745aa0de30c94cf
nightapple <nightapple126@gmail.com>
2022-06-09 22:42:37 +0800

    reat-work-220609

---
+++ Crypto tradesv1.3/admin/src/app/constants/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+    site_name: "Cryptotrades",
+    media_path: "http://localhost:3000",
+    base_url: "http://localhost:4200",
+    frontend_url: "http://localhost:4201"
+}
